9ae1db0b6873704fdc091efefcb3e4fc58e04be2
Archit Aggarwal <archit522@gmail.com>
2020-09-10 02:11:39 +0530

    Algorithm changes for answer in range [11, 19]

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 
-
+//
 function nearby(diff){
 	var c = diff.toString().length;
 	var result = [];
@@ -14,18 +14,18 @@ function nearby(diff){
 	for(var i=1; i<c; i++){
 		poss.push(100/Math.pow(10, i));
 	}
+	console.log(poss, diff);
 	while(result.length != 4){
 		lmax = poss.length-1;
 		lmin = 0;
 		var a = Math.floor(Math.random() * (lmax - lmin + 1) + lmin);
-		lmax = diff + Math.floor(poss[a]*diff/100);
-		lmin = diff - Math.floor(poss[a]*diff/100);
+		lmax = diff + Math.ceil(poss[a]*diff/100);
+		lmin = diff - Math.ceil(poss[a]*diff/100);
 		// console.log(lmax, lmin);
 		var b = Math.floor(Math.random() * (lmax - lmin + 1) + lmin);
-		if(b in result){
-			continue;
+		if(!result.includes(b)){
+			result.push(b);
 		}
-		result.push(b);
 	}
 	return result;
 }
@@ -37,10 +37,9 @@ function totalrandom(diff){
 	lmin = minit(c);
 	while(result.length != 4){
 		var a = Math.floor(Math.random() * (lmax - lmin + 1) + lmin);
-		if(a in result){
-			continue;
+		if(!result.includes(a)){
+			result.push(a);
 		}
-		result.push(a);
 	}
 	// a = Math.floor(Math.random() * (3 - 0 + 1) + 0);
 	// result[a] = diff;
@@ -48,9 +47,11 @@ function totalrandom(diff){
 }
 
 function compromise(diff){
-	var r1 = nearby(diff), r2 = totalrandom(diff), result = [], c = 0;
-	// console.log(r1);
-	// console.log(r2);
+	var r1 = nearby(diff);
+	console.log(r1);
+	var r2 = totalrandom(diff), result = [], c = 0;
+	console.log(r2);
+	console.log("----------------------");
 	if(r1.length == 0){
 		result = r2;
 	}
@@ -91,21 +92,36 @@ function maxit(p){
 }
 
 function minit(p){
+	if(p == 1){
+		return 0;
+	}
 	return Math.pow(10, p-1);
 }
 
 //Request to generate and return a question bank based on the given inputs and constraints 
 router.post('/', async (req, res) =>{
-	// console.log(req.body.question);
 	const f = req.body.minuend;
 	const g = req.body.subtrahend;
+	const flag = req.body.flag;
+	const q = req.body.question;
+	if(Number.isInteger(f) == false || f < 1 || f > 10){
+		res.json({message: "Incorrect minuend input"});
+	}
+	if(Number.isInteger(g) == false || g < 1 || g > 10 || f < g){
+		res.json({message: "Incorrect subtrahend input"});
+	}
+	if(!(Number.isInteger(flag) == true && (flag == 0 || flag == 1))){
+		res.json({message: "Incorrect flag input"});
+	}
+	if(Number.isInteger(q) == false || q < 0){
+		res.json({message: "Incorrect number of questions input"});
+	}
 	fmax = maxit(f);
 	fmin = minit(f);
 	gmax = maxit(g);
 	gmin = minit(g);
-	const flag = req.body.flag;
 	var m = [], s = [], op = [], ans = [];
-	for(var i = 1; i <= req.body.question; i++){
+	for(var i = 1; i <= q; i++){
 		a = Math.floor(Math.random() * (fmax - fmin + 1) + fmin);
 		b = Math.floor(Math.random() * (gmax - gmin + 1) + gmin);
 		if(b > a || checkit(a, b) != flag){
